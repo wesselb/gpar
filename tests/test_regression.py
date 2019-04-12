4963c14bdd3f9009d00a8204ce4e628703c58196
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-04-12 19:54:23 +0100

    Add test for _uprank and _vector_from_init

---
+++ tests/test_regression.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+from gpar.regression import _uprank, _vector_from_init
+
+# noinspection PyUnresolvedReferences
+from . import eq, neq, lt, le, ge, gt, raises, call, ok, lam, allclose, approx
+
+
+def test_uprank():
+    yield allclose, _uprank(1), np.ones((1, 1))
+    yield allclose, _uprank(np.ones(1)), np.ones((1, 1))
+    yield allclose, _uprank(np.ones((1, 1))), np.ones((1, 1))
+    yield raises, ValueError, lambda: _uprank(np.ones((1, 1, 1)))
+
+
+def test_vector_from_init():
+    yield allclose, _vector_from_init(2, 2), np.array([2, 2])
+    yield allclose, \
+          _vector_from_init(np.array([1, 2, 3]), 2), \
+          np.array([1, 2])
+    yield raises, \
+          ValueError, \
+          lambda: _vector_from_init(np.random.randn(2, 2), 1)
+    yield raises, \
+          ValueError, \
+          lambda: _vector_from_init(np.array([1, 2]), 3)
