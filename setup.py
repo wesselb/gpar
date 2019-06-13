93ecb9f2308c5234668d5c0c7d4082433ead32e5
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-06-13 18:55:20 +0100

    Tighten requirements and increase version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, division, print_function
 
 from setuptools import find_packages, setup
 
-requirements = ['numpy',
+requirements = ['numpy>=1.16',
                 'torch',
 
                 'plum-dispatch',
@@ -13,4 +13,5 @@ requirements = ['numpy',
                 'varz']
 
 setup(packages=find_packages(exclude=['docs']),
-      install_requires=requirements)
+      install_requires=requirements,
+      include_package_data=True)
