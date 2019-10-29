1d9b68b699cedd87e0eb0a5636fedb3f7b6fd98b
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-10-29 22:00:00 +0000

    Drop Python 2

--- setup.py
+++ setup.py
@@ -13,5 +13,6 @@ requirements = ['numpy>=1.16',
                 'varz']
 
 setup(packages=find_packages(exclude=['docs']),
+      python_requires='>=3.5',
       install_requires=requirements,
       include_package_data=True)
