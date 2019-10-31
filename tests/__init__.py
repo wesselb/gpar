8508d7f06c58c1bb007ed353f68e8fbfac9a8e39
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-10-31 17:43:51 +0000

    Remove TF import

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,9 +8,3 @@ import sys
 # Add package to path.
 file_dir = os.path.dirname(__file__)
 sys.path.insert(0, os.path.abspath(os.path.join(file_dir, '..')))
-
-# The below imports are there to not have CI fail.
-# noinspection PyUnresolvedReferences
-import torch
-# noinspection PyUnresolvedReferences
-import tensorflow
