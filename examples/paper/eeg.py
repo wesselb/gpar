69d1ba8e85c64009cfe914a36940cc6c4a5608b7
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-02-21 21:40:33 +0000

    Clean up EEG experiment

--- examples/paper/eeg.py
+++ examples/paper/eeg.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import wbml.metric
 import wbml.out
 import wbml.plot
 from lab import B
@@ -9,52 +10,48 @@ from wbml.experiment import WorkingDirectory
 
 from gpar import GPARRegressor
 
-B.epsilon = 1e-8
-wd = WorkingDirectory('_experiments', 'eeg')
+if __name__ == '__main__':
 
-_, train, test = load()
-y_labels = list(train.columns)
+    B.epsilon = 1e-8
+    wbml.out.report_time = True
+    wd = WorkingDirectory('_experiments', 'eeg')
 
-# Convert to NumPy.
-x = np.array(train.index)
-y_train = np.array(train)
-y_test = np.array(test)
+    _, train, test = load()
 
-# Fit and predict GPAR.
-model = GPARRegressor(scale=0.02,
-                      linear=False, nonlinear=True, nonlinear_scale=1.0,
-                      noise=0.01,
-                      impute=True, replace=False, normalise_y=True)
-model.fit(x, y_train)
-means, lowers, uppers = \
-    model.predict(x, num_samples=100, credible_bounds=True, latent=True)
+    x = np.array(train.index)
+    y = np.array(train)
 
-# Compute SMSE.
-pred = pd.DataFrame(means, index=train.index, columns=train.columns)
-smse = ((pred - test) ** 2).mean().mean() / \
-       ((test.mean(axis=0) - test) ** 2).mean().mean()
+    # Fit and predict GPAR.
+    model = GPARRegressor(scale=0.02,
+                          linear=False, nonlinear=True, nonlinear_scale=1.0,
+                          noise=0.01,
+                          impute=True, replace=False, normalise_y=True)
+    model.fit(x, y)
+    means, lowers, uppers = \
+        model.predict(x, num_samples=100, credible_bounds=True, latent=True)
 
-# Report and save average SMSE.
-wbml.out.kv('SMSE', smse)
+    # Report SMSE.
+    pred = pd.DataFrame(means, index=train.index, columns=train.columns)
+    smse = wbml.metric.smse(pred, test)
+    wbml.out.kv('SMSEs', smse.dropna())
+    wbml.out.kv('Average SMSEs', smse.mean())
 
-# Plot the result.
-plt.figure(figsize=(12, 9))
-wbml.plot.tex()
+    # Name of output to plot.
+    name = 'F2'
 
-for i, label in enumerate(y_labels[-3:]):
-    y_i = y_labels.index(label)
-    ax = plt.subplot(3, 1, i + 1)
-    plt.plot(x, means[:, y_i], c='tab:blue')
-    plt.fill_between(x, lowers[:, y_i], uppers[:, y_i],
-                     facecolor='tab:blue', alpha=.25)
-    plt.scatter(train.index, train[label], c='tab:green', marker='x', s=10)
-    plt.scatter(test.index, test[label], c='tab:orange', marker='x', s=10)
-    plt.xlabel('Time (seconds)')
-    plt.ylabel('Voltage (volt)')
-    plt.title(y_labels[y_i])
+    # Plot the result.
+    plt.figure(figsize=(12, 1.75))
+    wbml.plot.tex()
+
+    p = list(train.columns).index(name)
+    plt.plot(x, means[:, p], style='pred')
+    plt.fill_between(x, lowers[:, p], uppers[:, p], style='pred')
+    plt.scatter(x, y[:, p], style='train')
+    plt.scatter(test[name].index, test[name], style='test')
+    plt.xlabel('Time (second)')
     plt.xlim(0.4, 1)
+    plt.ylabel(f'{name} (volt)')
     wbml.plot.tweak(legend=False)
 
-plt.tight_layout()
-plt.savefig(wd.file('prediction.pdf'))
-plt.show()
+    plt.tight_layout()
+    plt.savefig(wd.file('eeg.pdf'))
