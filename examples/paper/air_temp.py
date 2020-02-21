0acbfd535671a57e0cf9fa6aa0232de61791cda0
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-02-21 21:37:36 +0000

    Clean up air temp experiment

--- examples/paper/air_temp.py
+++ examples/paper/air_temp.py
@@ -1,47 +1,54 @@
-import pickle
 import sys
 
 import numpy as np
-from gpar import GPARRegressor
+import wbml.out
 from lab import B
 from wbml.data.air_temp import load as load_temp
+from wbml.experiment import WorkingDirectory
 
-B.epsilon = 1e-6
-
-d_size = 0 if len(sys.argv) < 2 else int(sys.argv[1])
-d_all, d_train, d_tests = load_temp()[d_size]
-n_ind = [10 * 10 + 1, 10 * 15 + 1, 10 * 31 + 1][d_size]
+from gpar import GPARRegressor
 
 
-def convert_index(d):
-    index = d.index - d_all.index[0]
+def convert_index(df):
+    index = df.index - d_all.index[0]
     return np.array([td.total_seconds() / 3600 / 24 for td in index])
 
 
-# Place inducing points evenly spaced.
-x = convert_index(d_all)
-x_ind = np.linspace(x.min(), x.max(), n_ind)
+if __name__ == '__main__':
 
-# Fit and predict GPAR.
-#   Note: we use D-GPAR-L-NL here, as opposed to D-GPAR-L, to make the results
-#   a little more drastic.
-model = GPARRegressor(scale=0.2,
-                      linear=True, linear_scale=10.,
-                      nonlinear=True, nonlinear_scale=1.,
-                      noise=0.1,
-                      impute=True, replace=True, normalise_y=True,
-                      x_ind=x_ind)
-model.fit(convert_index(d_train), d_train.to_numpy())
+    B.epsilon = 1e-6
+    wbml.out.report_time = True
+    wd = WorkingDirectory('_experiments', 'air_temp')
 
-# Predict for the test sets.
-preds = []
-for i, d in enumerate(d_tests):
-    print('Sampling', i + 1)
-    preds.append(model.predict(convert_index(d),
-                               num_samples=50,
-                               credible_bounds=True,
-                               latent=False))
+    # Load data.
+    d_size = 0 if len(sys.argv) < 2 else int(sys.argv[1])
+    d_all, d_train, d_tests = load_temp()[d_size]
 
-# Save predictions.
-with open('examples/paper/air_temp_results{}.pickle'.format(d_size), 'wb') as f:
-    pickle.dump(preds, f)
+    # Determine the number of inducing points.
+    n_ind = [10 * 10 + 1, 10 * 15 + 1, 10 * 31 + 1][d_size]
+
+    # Place inducing points evenly spaced.
+    x = convert_index(d_all)
+    x_ind = np.linspace(x.min(), x.max(), n_ind)
+
+    # Fit and predict GPAR.
+    #   Note: we use D-GPAR-L-NL here, as opposed to D-GPAR-L, to make the
+    #   results a little more drastic.
+    model = GPARRegressor(scale=0.2,
+                          linear=True, linear_scale=10.,
+                          nonlinear=True, nonlinear_scale=1.,
+                          noise=0.1,
+                          impute=True, replace=True, normalise_y=True,
+                          x_ind=x_ind)
+    model.fit(convert_index(d_train), d_train.to_numpy())
+
+    # Predict for the test sets.
+    preds = []
+    for i, d in enumerate(d_tests):
+        preds.append(model.predict(convert_index(d),
+                                   num_samples=50,
+                                   credible_bounds=True,
+                                   latent=False))
+
+    # Save predictions.
+    wd.save(preds, f'results{d_size}.pickle')
--- examples/paper/air_temp_process.py
+++ examples/paper/air_temp_process.py
@@ -1,16 +1,22 @@
-import pickle
-
 import matplotlib.pyplot as plt
 import numpy as np
-from wbml.data.air_temp import load as load_temp
+from wbml.data.air_temp import load
+from wbml.experiment import WorkingDirectory
+import wbml.plot
+import wbml.metric
+import pandas as pd
 
 
 def date_to_day(dt):
     return dt.day + (dt.hour + (dt.minute + dt.second / 60) / 60) / 24
 
 
+wd = WorkingDirectory('_experiments', 'air_temp',
+                      log='log_process.txt',
+                      subtle=True)
+
 # Load data.
-data = load_temp()
+data = load()
 
 # Create lookups.
 lookup_place = {('temp', 'Chi'): 'Chimet', ('temp', 'Cam'): 'Cambermet'}
@@ -18,34 +24,25 @@ lookup_size = {0: '10 Days', 1: '15 Days', 2: '1 Month'}
 
 # Plot the results.
 plt.figure(figsize=(15, 4))
-plt.rcParams['font.family'] = 'serif'
-plt.rcParams['mathtext.fontset'] = 'dejavuserif'
 
 for d_size in [0, 1, 2]:
     d_all, d_train, d_tests = data[d_size]
 
     # Load predictions.
-    with open(f'examples/paper/air_temp_results{d_size}.pickle', 'rb') as f:
-        preds = pickle.load(f)
+    preds = wd.load(f'results{d_size}.pickle')
 
     # Compute SMSEs for the first two data sets; the others are the extended
     # ones.
     smses = []
-    for (mean, _, _), d in list(zip(preds, d_tests))[:2]:
-        y_i = list(d_train.columns).index(d.columns[0])
-        mse_mean = \
-            np.nanmean((d - np.nanmean(d, axis=0, keepdims=True)) ** 2)
-        mse_gpar = np.nanmean((d - mean[:, y_i:y_i + 1]) ** 2)
-        smses.append(mse_gpar / mse_mean)
+    for (mean, _, _), d_test in list(zip(preds, d_tests))[:2]:
+        mean = pd.DataFrame(mean, index=d_test.index, columns=d_train.columns)
+        smse = wbml.metric.smse(mean, d_test).mean()
+        smses.append(smse)
     smse = np.mean(smses)
 
     # Construct plots.
     for i, (mean, lowers, uppers) in enumerate(preds[2:]):
-        ax = plt.subplot(2, 3, d_size + i * 3 + 1)
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-        ax.yaxis.set_ticks_position('left')
-        ax.xaxis.set_ticks_position('bottom')
+        plt.subplot(2, 3, d_size + i * 3 + 1)
 
         # Extract test set and extended test set.
         d_test, d_test_ext = d_tests[i], d_tests[i + 2]
@@ -54,26 +51,25 @@ for d_size in [0, 1, 2]:
 
         # Plot prediction.
         y_i = list(d_train.columns).index(d_test.columns[0])
-        plt.plot(x_ext, mean[:, y_i], c='tab:blue')
-        plt.fill_between(x_ext, lowers[:, y_i], uppers[:, y_i],
-                         facecolor='tab:blue', alpha=.25)
+        plt.plot(x_ext, mean[:, y_i], style='pred')
+        plt.fill_between(x_ext, lowers[:, y_i], uppers[:, y_i], style='pred')
 
         # Plot data.
-        plt.scatter(x_ext, d_test_ext, c='tab:green', s=2)
-        plt.scatter(x, d_test, c='tab:orange', s=2)
+        plt.scatter(x_ext, d_test_ext, style='train', s=4)
+        plt.scatter(x, d_test, style='test', s=4)
 
         # Finalise plot.
         plt.xlim(x_ext[0], x_ext[-1])
         plt.ylim(10, 30)
         plt.yticks([15, 20, 25])
         if i == 0:
-            plt.title(lookup_size[d_size] + ' (SMSE: {:.3f})'.format(smse))
+            plt.title(lookup_size[d_size] + f' (SMSE: {smse:.3f})')
         if i == 1:
             plt.xlabel('Time (day)')
         if d_size == 0:
-            plt.ylabel('{}\nTemp. (celsius)'
-                       ''.format(lookup_place[d_test.columns[0]]))
+            plt.ylabel(f'{lookup_place[d_test.columns[0]]}\nTemp. (celsius)')
+
+        wbml.plot.tweak(legend=False)
 
 plt.tight_layout()
-plt.savefig('examples/paper/air_temp_prediction.pdf')
-plt.show()
+plt.savefig(wd.file('air_temp.pdf'))
